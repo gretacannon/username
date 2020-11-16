8489243ff02133ff2a6ac74d5da656b6cbebb329
Greta Cannon <grcannon@chapman.edu>
2020-11-16 10:26:17 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -5,7 +5,7 @@ echo "Enter a username: "
 echo "The user name must start with a letter and include only digits, lowercase letters,and an underscore."
 echo "It must also be at least 3 characters and no more than 12"
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][abcdefghijklmnopqrstuvwxyz0123456789_]{3,12}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][abcdefghijklmnopqrstuvwxyz0123456789_]{2,11}$" > /dev/null 2>&1
 do
 	echo "That is not valid: "
 	echo "Enter a username: "
